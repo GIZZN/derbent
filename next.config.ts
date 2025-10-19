55c18eabe485faf976cc4b83adb22fa91a257006
sosal <sosal>
2025-10-19 14:01:06 +0300

    first commit

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.vchugaev.ru',
+        port: '',
+        pathname: '/smartmatch/**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
